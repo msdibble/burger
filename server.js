d1c55b430a086be9429821c8fabd9017c7cca4cc
unknown <mdibble93@gmail.com>
2018-05-16 23:43:22 -0400

    server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Content from the "public" directory
+app.use(express.static("public"));
+
+// Parse full objects
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Parse json
+app.use(bodyParser.json());
+
+// Requrie handlebars package
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Importing the routes
+var routes = require("./controllers/burgers_controllers.js");
+
+app.use(routes);
+
+// Tell the app to listen to client requests
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
+
