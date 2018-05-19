9823df3f0cb9b34c24ae32188d03700b8cb3fb6e
unknown <mdibble93@gmail.com>
2018-05-18 22:45:41 -0400

    img

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,3 +1,45 @@
 $(document).ready(function(){
-    
+    $(function(){
+        $(".change-devoured").on("click", function(event){
+            var id = $(this).data("id");
+            var newDevoured = $(this).data("newDevoured");
+
+            var devouredState = {
+                devoured: newDevoured
+            };
+
+            // Send PUT request
+            $.ajax("/api/burgers" + id, {
+                type: "PUT",
+                data: devouredState
+            }).then(
+                function(){
+                    console.log("changed devoured to", newDevoured);
+                    // Reloading the page for the updated list
+                    location.reload();
+                }
+            )
+        });
+
+        $(".create-update-form").on("submit", function(event){
+            event.peventDefault();
+
+            var newBurger = {
+                name: $("#burgName").val().trim(),
+                devoured: $("[name=devoured]:checked").val().trim()
+            };
+
+            //Send POST request
+            $.ajax("/api/burgers", {
+                type: "POST",
+                data: newBurger
+            }).then(
+                function(){
+                    console.log("created new burger");
+
+                    location.reload;
+                }
+            )
+        });
+    });  
 }); 
