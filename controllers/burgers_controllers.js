aad1a69065c9b343d024f7caf3a435818e334dc7
unknown <mdibble93@gmail.com>
2018-05-18 22:06:17 -0400

    burgers_controllers.js

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -4,13 +4,53 @@ var router = express.Router();
 // Importing burger.js model to use the database functions
 var burger = require("../models/burger.js");
 
-router.get()
+router.get("/", function(req, res){
+    burger.allBurgers(function(data){
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
 
-router.post()
+router.post("/api/burgers", function(req, res){
+    burger.insertOne([
+        "burger_name"
+    ], [
+        req.body.name
+    ], function(results){
+        res.json({ id: results.insertId });
+    });
+});
 
+router.put("/api/burgers/:id", function(req, res){
+    var condition = "id = " + req.params.id;
 
-router.delete()
+    console.log("condition", condition);
+
+    burger.update({ devoured: req.body.devoured}, condition, function(results) {
+        if(results.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/burgers/:id", function(req, res){
+    var condition = "id = " + req.params.id;
+
+    burger.delete(condition, function(results){
+        if(results.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+
+});
 
 // Exporting routes
 module.exports = router;
